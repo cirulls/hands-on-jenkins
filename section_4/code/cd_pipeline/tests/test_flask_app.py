696a0e2609cc2066ceb7d23d6e506de0649a2b3b
Sandro Cirulli <sandro.cirulli@oup.com>
2018-03-31 20:07:54 +0100

    Add flask test

---
+++ section_4/code/cd_pipeline/tests/test_flask_app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import unittest
+import app
+
+class BasicTests(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.app.test_client()
+
+    # executed after each test
+    def tearDown(self):
+        pass
+ 
+ 
+###############
+#### tests ####
+###############
+ 
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+ 
+ 
+if __name__ == "__main__":
+    unittest.main()
