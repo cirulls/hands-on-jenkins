e8f321104c256f5b2ff79bf9f217316e60350e1b
Sandro Cirulli <sandro.cirulli@oup.com>
2018-04-09 07:43:08 +0100

    Add tests

---
+++ section_4/code/cd_pipeline/tests/runUAT.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+hostname='localhost'
+port=$1
+
+status_code=$(curl --write-out %{http_code} --out /dev/null --silent ${hostname}:${port})
+
+if [ $status_code == 200 ];
+then
+	echo "PASS: ${hostname}:${port} is reachable"
+else
+	echo "FAIL: ${hostname}:${port} is unreachable"
+    exit 1
+fi
