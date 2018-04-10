ba14724ae60921d9a7d00acd985a663bdff81566
Sandro Cirulli <sandro.cirulli@oup.com>
2018-04-10 14:27:18 +0100

    Add documentation

--- section_4/code/cd_pipeline/tests/test_flask_app.py
+++ section_4/code/cd_pipeline/tests/test_flask_app.py
@@ -2,12 +2,12 @@
 
 import sys
 import os
-sys.path.append(os.environ['WORKSPACE'])
 import unittest
 import app
 
 class BasicTests(unittest.TestCase):
 
+    # execute before each test
     def setUp(self):
         self.app = app.app.test_client()
 
@@ -15,15 +15,10 @@ class BasicTests(unittest.TestCase):
     def tearDown(self):
         pass
  
- 
-###############
-#### tests ####
-###############
- 
+    # tests 
     def test_main_page(self):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
- 
  
 if __name__ == "__main__":
     unittest.main()
