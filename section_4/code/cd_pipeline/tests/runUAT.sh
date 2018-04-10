dfdc01fc04b34ff0f221412f20513c832d0092eb
Sandro Cirulli <sandro.cirulli@oup.com>
2018-04-10 14:21:25 +0100

    Add sleep

--- section_4/code/cd_pipeline/tests/runUAT.sh
+++ section_4/code/cd_pipeline/tests/runUAT.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# set variables
 hostname='localhost'
 port=$1
 
+# wait for the app to start
+sleep 5 
+
+# ping the app
 status_code=$(curl --write-out %{http_code} --out /dev/null --silent ${hostname}:${port})
 
 if [ $status_code == 200 ];
