226eb852119c454af197a54a6809f5b1c69e582e
Daniel Chicot <dan.chicot@gmail.com>
2019-04-26 10:24:36 +0100

    Add python dependencies as non-root.

--- section_4/code/cd_pipeline/tests/runUAT.sh
+++ section_4/code/cd_pipeline/tests/runUAT.sh
@@ -5,15 +5,15 @@ hostname='localhost'
 port=$1
 
 # wait for the app to start
-sleep 5 
+sleep 5
 
 # ping the app
 status_code=$(curl --write-out %{http_code} --out /dev/null --silent ${hostname}:${port})
 
 if [ $status_code == 200 ];
 then
-	echo "PASS: ${hostname}:${port} is reachable"
+    echo "PASS: ${hostname}:${port} is reachable"
 else
-	echo "FAIL: ${hostname}:${port} is unreachable"
+    echo "FAIL: ${hostname}:${port} is unreachable"
     exit 1
 fi
--- section_4/code/cd_pipeline/tests/test_flask_app.py
+++ section_4/code/cd_pipeline/tests/test_flask_app.py
@@ -14,11 +14,11 @@ class BasicTests(unittest.TestCase):
     # executed after each test
     def tearDown(self):
         pass
- 
-    # tests 
+
+    # tests
     def test_main_page(self):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
- 
+
 if __name__ == "__main__":
     unittest.main()
